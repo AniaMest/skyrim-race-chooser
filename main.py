57849d0308ddc4e361c71d16e0497fc41ce5e9ea
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-03-14 03:40:58 +0400

    main.py updated
    Initial draft of evaluate_races() is added.

--- main.py
+++ main.py
@@ -10,11 +10,20 @@ def evaluate_races(races, chosen_skills):
     scores = {}
     for race in races:
         scores[race] = 0
+
+    for skill in chosen_skills:
+        for race, race_skill_value in races.items():
+
+            skill_value = race_skill_value.get(skill, 15)
+
+            if skill_value == 25:
+                scores[race] += 3
+            elif skill_value == 20:
+                scores[race] += 2
+            else: 
+                scores[race] += 1
     return scores
-#   for skill in chosen_skills:
-#       for race, race_skills in races.items():
-            
 
-    
+scores = evaluate_races(races, chosen_skills)  
 
-print(races.items())
+print(scores)
