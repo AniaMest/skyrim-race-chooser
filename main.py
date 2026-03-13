3941a3ff18d1e83e20da4af472cf4000cc38837d
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-03-14 03:19:03 +0400

    change in races.json
    Default skill values (15s) have been omitted from arrays.
    20 and 25 points remain.

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import json
 
 with open("races.json") as f:
     races = json.load(f)
+
+chosen_skills = {"Conjuration", "Light Armor",  "Speech",  "Smithing",  "Lockpicking"}
+
+def evaluate_races(races, chosen_skills):
+
+    scores = {}
+    for race in races:
+        scores[race] = 0
+    return scores
+#   for skill in chosen_skills:
+#       for race, race_skills in races.items():
+            
+
+    
+
+print(races.items())
