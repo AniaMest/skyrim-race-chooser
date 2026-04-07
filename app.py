d33fa55a586b7740ad6177d23c3e8b0117a2c12c
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-04-07 19:05:45 +0400

    made the buttons functional

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/race/<name>")
+def race(name):
+    return f"{name}"
+
 if __name__ == "__main__":
     app.run(debug=True)
