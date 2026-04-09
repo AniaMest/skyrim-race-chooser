1e9a5d8d3ba44885642daf7374f23b5d07ad4fda
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-04-09 20:42:44 +0400

    Small changes in app.py
    function name changed. quiz() added

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/race/<name>")
 def race(name):
     return f"{name}"
+
+@app.route("/quiz")
+def quiz():
+    return render_template("quiz.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
